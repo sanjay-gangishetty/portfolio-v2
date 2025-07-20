4285968ff4198a32f81328773cf127fef96c7429
sanjay-gangishetty <gangishettysanjaykumar@gmail.com>
2025-07-20 15:06:39 +0530

    Update: footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Icon } from '@components/icons';
@@ -68,26 +68,6 @@ const StyledCredit = styled.div`
 `;
 
 const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
-
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
 
   return (
     <StyledFooter>
@@ -104,24 +84,11 @@ const Footer = () => {
         </ul>
       </StyledSocialLinks>
 
-      {/* <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
-
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
+      <StyledCredit tabindex="-1">
+        <a href="https://github.com/sanjay-gangishetty">
+          <div>© 2025 Sanjay Kumar. All rights reserved.</div>
         </a>
-      </StyledCredit> */}
+      </StyledCredit>
     </StyledFooter>
   );
 };

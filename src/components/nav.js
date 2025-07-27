b41d0b9d6cdc3a373965e02172fea13bd82c02fd
sanjay-gangishetty <gangishettysanjaykumar@gmail.com>
2025-07-27 09:35:30 +0530

    fix: resume links.

--- src/components/nav.js
+++ src/components/nav.js
@@ -206,7 +206,7 @@ const Nav = ({ isHome }) => {
   );
 
   const ResumeLink = (
-    <a className="resume-button" href="/GangishettySanjayKumar.pdf" target="_blank" rel="noopener noreferrer">
+    <a className="resume-button" href="GangishettySanjayKumar.pdf" target="_blank" rel="noopener noreferrer">
       Resume
     </a>
   );
